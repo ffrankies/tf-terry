d5ed297d9a57c9897ddcdacf19c62732d02697f1
ffrankies <wanyef@mail.gvsu.edu>
2017-10-13 16:52:04 -0400

    Fixed issue where last batch wasn't padded vertically

--- tests/test_batchmaker.py
+++ tests/test_batchmaker.py
@@ -57,7 +57,8 @@ BATCHED_SORTED_DATA_3 = [
     ],
     [
         [0, 1],
-        [0]
+        [0],
+        []
     ]
 ]
 
@@ -93,7 +94,8 @@ BATCHED_SCRAMBLED_DATA_3 = [
     ],
     [
         [0, 1, 2, 3, 4, 5, 6],
-        [0, 1, 2, 3, 4, 5]
+        [0, 1, 2, 3, 4, 5],
+        []
     ]
 ]
 
@@ -155,12 +157,44 @@ TRUNCATED_SORTED_BATCHES_4 = [
     ]
 ]
 
+TRUNCATED_SORTED_BATCHES_3_4 = [
+    [
+        [0, 1, 2, 3],
+        [0, 1, 2, 3],
+        [0, 1, 2, 3]
+    ],
+    [
+        [4, 5, 6, 7],
+        [4, 5, 6],
+        [4, 5]
+    ],
+    [
+        [0, 1, 2, 3],
+        [0, 1, 2, 3],
+        [0, 1, 2]
+    ],
+    [
+        [4],
+        [],
+        []
+    ],
+    [
+        [0, 1],
+        [0],
+        []
+    ]
+]
+
 SIZES_TRUNCATED_SCRAMBLED_BATCHES_4 = [
     [4, 3], [4, 0], [2, 4], [0, 1], [4, 1], [4, 4], [3, 2]
 ]
 
 SIZES_TRUNCATED_SORTED_BATCHES_4 = [
     [4, 4], [4, 3], [4, 4], [2, 1], [4, 3], [2, 1]
+]
+
+SIZES_TRUNCATED_SORTED_BATCHES_3_4 = [
+    [4, 4, 4], [4, 3, 2], [4, 4, 3], [1, 0, 0], [2, 1, 0]
 ]
 
 PADDED_SCRAMBLED_BATCHES_4 = [
@@ -221,10 +255,38 @@ PADDED_SORTED_BATCHES_4 = [
     ]
 ]
 
+PADDED_SORTED_BATCHES_3_4 = [
+    [
+        [0, 1, 2, 3],
+        [0, 1, 2, 3],
+        [0, 1, 2, 3]
+    ],
+    [
+        [4, 5, 6, 7],
+        [4, 5, 6, PAD],
+        [4, 5, PAD, PAD]
+    ],
+    [
+        [0, 1, 2, 3],
+        [0, 1, 2, 3],
+        [0, 1, 2, PAD]
+    ],
+    [
+        [4, PAD, PAD, PAD],
+        [PAD, PAD, PAD, PAD],
+        [PAD, PAD, PAD, PAD]
+    ],
+    [
+        [0, 1, PAD, PAD],
+        [0, PAD, PAD, PAD],
+        [PAD, PAD, PAD, PAD]
+    ]
+]
+
 class TestSortByLength():
     def test_should_not_change_data_when_it_is_empty(self):
         assert sort_by_length([]) == []
-    
+
     def test_should_not_change_data_when_it_is_already_sorted(self):
         assert sort_by_length([SORTED_DATA]) == [SORTED_DATA]
 
@@ -238,11 +300,11 @@ class TestSortByLength():
 class TestGroupIntoBatches():
     def test_should_return_empty_list_when_data_is_empty(self):
         assert group_into_batches([], 4) == []
-    
+
     def test_should_raise_value_error_when_batch_size_is_less_than_one(self):
         with pytest.raises(ValueError):
             group_into_batches([], 0)
-    
+
     def test_should_not_raise_value_error_when_batch_size_is_one(self):
         group_into_batches([], 1)
         pass
@@ -257,7 +319,7 @@ class TestGroupIntoBatches():
 class TestTruncateBatches():
     def test_should_do_nothing_when_data_is_empty(self):
         assert truncate_batches([], 2) == []
-    
+
     def test_should_raise_value_error_when_truncate_is_less_than_one(self):
         with pytest.raises(ValueError):
             truncate_batches([], 0)
@@ -278,6 +340,7 @@ class TestTruncateBatches():
 
     def test_should_correctly_truncate_data(self):
         assert truncate_batches([BATCHED_SCRAMBLED_DATA_2], 4) == [TRUNCATED_SCRAMBLED_BATCHES_4]
+        assert truncate_batches([BATCHED_SORTED_DATA_3], 4) == [TRUNCATED_SORTED_BATCHES_3_4]
 
     def test_should_correctly_work_with_multiple_data(self):
         assert truncate_batches([BATCHED_SCRAMBLED_DATA_2, BATCHED_SORTED_DATA_2], 4) == [
@@ -286,17 +349,18 @@ class TestTruncateBatches():
 class TestGetRowLenghts():
     def test_should_return_empty_list_when_data_is_empty(self):
         assert get_row_lengths([]) == []
-    
+
     def test_should_return_row_lengths_for_one_piece_of_data(self):
         assert get_row_lengths(TRUNCATED_SORTED_BATCHES_4) == SIZES_TRUNCATED_SORTED_BATCHES_4
         assert get_row_lengths(TRUNCATED_SCRAMBLED_BATCHES_4) == SIZES_TRUNCATED_SCRAMBLED_BATCHES_4
-            
+        assert get_row_lengths(TRUNCATED_SORTED_BATCHES_3_4) == SIZES_TRUNCATED_SORTED_BATCHES_3_4
+
 class TestPadBatches():
     def test_should_return_empty_list_when_data_is_empty(self):
-        assert pad_batches([], 2, PAD) == []
-    
+        assert pad_batches([], 2, PAD).tolist() == []
+
     def test_should_throw_value_error_when_truncate_is_less_than_one(self):
-        with pytest.raises(ValueError):   
+        with pytest.raises(ValueError):
             pad_batches([], 0, PAD)
 
     def test_should_not_throw_value_error_when_truncate_is_one(self):
@@ -304,21 +368,25 @@ class TestPadBatches():
         pass
 
     def test_should_correctly_pad_data(self):
-        assert pad_batches(TRUNCATED_SCRAMBLED_BATCHES_4, 4, PAD) == PADDED_SCRAMBLED_BATCHES_4
-        assert pad_batches(TRUNCATED_SORTED_BATCHES_4, 4, PAD) == PADDED_SORTED_BATCHES_4
+        assert pad_batches(TRUNCATED_SCRAMBLED_BATCHES_4, 4, PAD).tolist() == PADDED_SCRAMBLED_BATCHES_4
+        assert pad_batches(TRUNCATED_SORTED_BATCHES_4, 4, PAD).tolist() == PADDED_SORTED_BATCHES_4
+        assert pad_batches(TRUNCATED_SORTED_BATCHES_3_4, 4, PAD).tolist() == PADDED_SORTED_BATCHES_3_4
 
 class TestMakeBatches():
     def test_should_do_nothing_when_data_is_empty(self):
-        assert make_batches([], [], 2, 4, PAD) == ([], [], [])
+        x_batches, y_batches, lengths = make_batches([], [], 2, 4, PAD)
+        assert x_batches.tolist() == []
+        assert y_batches.tolist() == []
+        assert lengths == []
 
     def test_should_raise_value_error_when_batch_size_is_less_than_one(self):
         with pytest.raises(ValueError):
             make_batches([], [], -1, 4, PAD)
-    
+
     def test_should_not_raise_value_error_when_batch_size_is_one(self):
         make_batches([], [], 1, 4, PAD)
         pass
-    
+
     def test_should_raise_value_error_when_truncate_is_less_than_one(self):
         with pytest.raises(ValueError):
             make_batches([], [], 2, -1, PAD)
@@ -335,6 +403,6 @@ class TestMakeBatches():
         for batch in y_batches:
             for example in batch:
                 assert len(example) <= 4
-        assert x_batches == PADDED_SORTED_BATCHES_4
-        assert y_batches == TRUNCATED_SORTED_BATCHES_4
+        assert x_batches.tolist() == PADDED_SORTED_BATCHES_4
+        assert y_batches.tolist() == PADDED_SORTED_BATCHES_4
         assert lengths == SIZES_TRUNCATED_SORTED_BATCHES_4
